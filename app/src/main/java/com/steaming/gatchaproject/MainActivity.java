cec7c9c13b36b5cfa4f6da8bc04d97e764f88c86
Choda79 <chodajw@gmail.com>
2021-12-19 23:07:36 +0900

    version 1.1

--- app/src/main/java/com/steaming/gatchaproject/MainActivity.java
+++ app/src/main/java/com/steaming/gatchaproject/MainActivity.java
@@ -3,12 +3,25 @@ package com.steaming.gatchaproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btn1 = (Button)findViewById(R.id.button);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "버튼클릭됨", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
     }
 }
